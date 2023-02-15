cc56eee64d39e115634fda4d3cc14c74d58f9765
Khoirur Rozaq <rozaqarmedian@gmail.com>
2023-02-15 13:57:24 +0700

    update api chatgpt

--- index.js
+++ index.js
@@ -1,92 +1,85 @@
-import makeWASocket, { makeWALegacySocket, useSingleFileAuthState, DisconnectReason } from '@adiwajshing/baileys';
+import makeWASocket, { useMultiFileAuthState, DisconnectReason } from '@adiwajshing/baileys';
 
 import { Boom } from '@hapi/boom';
-const { state, saveState } = useSingleFileAuthState('./auth_info.json');
-
-import app from 'express';
-import http from 'http';
+import { Configuration, OpenAIApi } from 'openai'
 import ChatGPT from './lib/chatGPT-lib.js';
 import config from './config.json' assert { type: 'json' };
 
-// create http server
-const server = http.createServer(app);
+const configuration = new Configuration({
+	apiKey: config.openaiKey,
+});
+const openai = new OpenAIApi(configuration);
 
-const port = process.env.PORT || 8000;
-
-//fungsi suara capital
-function capital(textSound) {
-	const arr = textSound.split(' ');
-	for (var i = 0; i < arr.length; i++) {
-		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-	}
-	const str = arr.join(' ');
-	return str;
-}
 
 async function connectToWhatsApp() {
+	const { state, saveCreds } = await useMultiFileAuthState('./auth_info');
 	const waConfig = {
 		auth: state,
 		printQRInTerminal: true
 	};
 
 	const sock = makeWASocket.default(waConfig);
+	
+	sock.ev.process(
+		async(events) => {
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
 
-	sock.ev.on('connection.update', (update) => {
-		//console.log(update);
-		const { connection, lastDisconnect } = update;
-		if (connection === 'close') {
-			const shouldReconnect = (lastDisconnect.error = Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
-			console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect);
-			// reconnect if not logged out
-			if (shouldReconnect) {
-				connectToWhatsApp();
-			}
-		} else if (connection === 'open') {
-			console.log('opened connection');
-		}
-	});
-
-	sock.ev.on('creds.update', saveState);
-
-	sock.ev.on('messages.upsert', async ({ messages, type }) => {
-		//console.log(messages);
-
-		if (type === 'notify') {
-			if (!messages[0].key.fromMe) {
-				// message declaration as text
-				const pesan = messages[0].message.conversation;
-				// wa phone number from sender
-				const noWa = messages[0].key.remoteJid;
-
-				// read message
-				await sock.readMessages([messages[0].key]);
-
-				// convert to lowercase
-				const pesanMasuk = pesan.toLowerCase();
-
-				if (!messages[0].key.fromMe && pesanMasuk === 'ping') {
-					await sock.sendMessage(noWa, { text: 'Pong' }, { quoted: messages[0] });
+				if(connection === 'close') {
+					// reconnect if not logged out
+					const shouldReconnect = (lastDisconnect.error = Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
+					console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect);
+					// reconnect if not logged out
+					if (shouldReconnect) {
+						connectToWhatsApp();
+					}
 				}
 
-				const resultGPT = await askGPT(pesanMasuk);
-				await sock.sendMessage(noWa, { text: resultGPT }, { quoted: messages[0] });
+				console.log('connection update', update)
+			}
 
+			// credentials updated -- save them
+			if(events['creds.update']) {
+				await saveCreds()
+			}
+
+			// messages received
+			if(events['messages.upsert']) {
+				const upsert = events['messages.upsert']
+				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+
+				// message received not group
+				if(upsert.messages[0].key.participant === undefined) {
+					// message declaration as text
+	 				const pesan = await upsert.messages[0].message.conversation;
+					// read messages
+					await sock.readMessages(upsert.messages.map(m => m.key))
+					// convert to lowercase
+					const pesanMasuk = pesan.toLowerCase();
+					// if message contains 'ping'
+					if(pesanMasuk.includes('ping')) {
+						// send a reply
+						await sock.sendMessage(upsert.messages[0].key.remoteJid, { text: 'Pong' }, { quoted: upsert.messages[0] })
+					} else {
+						// send a gpt
+						const resultGPT = await askGPT(pesanMasuk);
+						await sock.sendMessage(upsert.messages[0].key.remoteJid, { text: resultGPT }, { quoted: upsert.messages[0] });
+					}
+				}
 			}
 		}
-	});
+	)
+	
+	return sock;
+
 }
 
-async function askGPT(text) {
-	const chatbot = new ChatGPT(config);
-	let answer = await chatbot.ask(text);
 
-	console.log(answer);
-	return answer;
+async function askGPT(text) {
+	const chatbot = new ChatGPT(openai);
+	return chatbot.ask(text);
 }
 
 // run in main file
 connectToWhatsApp().catch((err) => console.log('unexpected error: ' + err)); // catch any errors
-
-server.listen(port, () => {
-	console.log('Server Berjalan pada Port : ' + port);
-});
